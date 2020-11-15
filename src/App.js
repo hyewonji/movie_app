60549cf656dbc6cedc5aa40aa1104255781ecaa8
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-15 23:42:40 +0900

    feat: redering components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function App() {
-  return <div>Hello!!!</div>;
+function Potato() {
+  return <h3>I like Potato</h3>;
 }
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!!!!</h1>
+      <Potato />
+    </div >
+  );
+}
+// jsx : html에 맞게 작성해줘야 한다.
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+//React apllication은 한 번에 하나의 component만 rendering 할 수 있다.
+//'App'파일에서 'App' component만 rendering한 상태
