3e34524d5184a4f86f80aae539ab371dd8342974
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-29 23:47:13 +0900

    feat: fetch movies from api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 
 class App extends React.Component {
@@ -5,11 +6,14 @@ class App extends React.Component {
     isLoading: true,
     movies: []
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 6000);
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
   }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
     const { isLoading } = this.state;
     return <div>{this.state.isLoading ? "Loading.." : "We are ready"}</div>
